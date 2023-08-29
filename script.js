6cd4d3bcd3fe8b420282f7a5d16f40377c282f24
khan <khan@codewithgit.com>
2023-08-30 04:10:33 +0530

    first Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let Myjokes=[" ordered a chicken and an egg from Amazon. I'll let you know." ,
+"What month is the shortest of the year? May, it only has three letters.",
+"What did the snail who was riding on the turtle's back say? Wheeeee!",
+"I was going to tell a time traveling joke, but you guys didn't like it.",
+"What do you call a lazy kangaroo? A pouch potato.",
+"I used to run a dating service for chickens, but I was struggling to make hens meet.",
+"Why do we tell actors to 'break a leg?' Because every play has a cast.",
+"What does a pig put on dry skin? Oinkment.","What do you call it when a snowman throws a tantrum? A meltdown.",
+"My uncle named his dogs Timex and Rolex. They're his watch dogs.",
+"Did you hear about the guy whose left side was cut off? He's all right now.",
+"How do you open a banana? With a mon-key."]
+let index=Math.floor(Math.random()*(0,Myjokes.length-1));
+joke.innerHTML=Myjokes[index]
